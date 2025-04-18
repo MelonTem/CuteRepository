e17dd9e3b404df508afca0e9a50a09444719fd6a
MelonTem <melontem3575@gmail.com>
2025-04-19 00:11:22 +0900

    강제푸시!!

---
+++ src/testPackage/ScannExTest.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+import java.util.Scanner;
+
+public class ScannExTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sexc = new Scanner(System.in);
+		
+		int i = sexc.nextInt();
+		
+		System.out.print(i);
+		
+		sexc.close();
+	}
+
+}
---
+++ src/testPackage/testClasse.java
@@ -0,0 +1,9 @@
+package testPackage;
+
+public class testClasse {
+
+	public static void main(String[] args) {
+		System.out.println("SANS");
+	}
+
+}
